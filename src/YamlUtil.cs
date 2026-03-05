1c0257e1302990fb208ced5866997d24f4b6ab03
Jake Soenneker <jake@soenneker.com>
2026-03-05 17:27:17 -0600

    misc

--- src/YamlUtil.cs
+++ src/YamlUtil.cs
@@ -29,7 +29,7 @@ public sealed class YamlUtil : IYamlUtil
                                                                                    .IgnoreUnmatchedProperties()
                                                                                    .Build();
 
-    private IFileUtil _fileUtil;
+    private readonly IFileUtil _fileUtil;
 
     public YamlUtil(IFileUtil fileUtil)
     {
